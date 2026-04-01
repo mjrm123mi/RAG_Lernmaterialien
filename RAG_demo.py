8bafb8cbb140bd66beb77e67692b2ea5b8a3ade1
Maryam Mirza <Maryam.Mirza@Student.HTW-Berlin.de>
2026-04-01 21:35:32 +0200

    Promt angepasst, damit das Modell weniger halluziniert. Chunkgröße verkleinert auf 500 und Chunkoverlap auf 150 verkleinert.

--- RAG_demo.py
+++ RAG_demo.py
@@ -1,6 +1,6 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-#from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_ollama import OllamaEmbeddings
 from langchain_chroma import Chroma
 from ollama import chat
@@ -11,7 +11,7 @@ from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
 docs = PyPDFLoader("Data/windows-whats-new.pdf").load()
 
 # split the document into chunks
-splitter = RecursiveCharacterTextSplitter(chunk_size=5000, chunk_overlap=1500)
+splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=150)
 chunks = splitter.split_documents(docs)
 
 # embed the chunks and store them in chroma db
@@ -21,7 +21,7 @@ vectorstore = Chroma.from_documents(chunks, embeddings, persist_directory="chrom
 
 # retrieve the most relevant chukns from the vectorstore
 def retrieve(query: str) -> str:
-    results = vectorstore.similarity_search(query, k=3)
+    results = vectorstore.similarity_search(query, k=5)
     return "\n\n".join([doc.page_content for doc in results])
 
 
@@ -29,9 +29,21 @@ def generate_answer(query: str, context: str) -> str:
     response = chat(
         model="gemma2:2b",
         messages=[
+
+            {
+                "role": "system",
+                "content": (
+                    "Du beantwortest Fragen ausschließlich anhand des bereitgestellten Kontexts. "
+                    "Wenn die Antwort nicht eindeutig im Kontext steht, antworte exakt mit: "
+                    "'Ich weiß es nicht'. Erfinde nichts und nutze kein Weltwissen."
+                )
+            },
+
             {"role": "user", "content": f"Answer the question based on the context provided.\n\nContext: {context}"},
             {"role": "user", "content": query}
-        ]
+
+        ],
+        options={"temperature": 0} #temperatur auf 0, damit wenige kreative Ergänzungen
     )
     return response["message"]["content"]
 
